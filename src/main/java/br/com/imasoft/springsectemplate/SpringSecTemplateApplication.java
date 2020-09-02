b581e7c39bacdf4dc0f2c8bb421edcc3beeea40a
Bruno Carneiro <bruno.souza@grancursosonline.com.br>
2020-09-02 15:09:42 -0300

    setup

---
+++ src/main/java/br/com/imasoft/springsectemplate/SpringSecTemplateApplication.java
@@ -0,0 +1,13 @@
+package br.com.imasoft.springsectemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/imasoft/springsectemplate/SpringSecTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imasoft.springsectemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
